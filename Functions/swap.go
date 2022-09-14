7812941206027697b3eec1d431ef21870853b009
Manoj Kumar <manokumar2684@gmail.com>
2022-09-14 06:14:12 +0530

    Adding Swap

---
+++ Functions/swap.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	var b int = 20
+	fmt.Println("Before SWAP value of A:", a)
+	fmt.Println("Before SWAP value of B:", b)
+	swap(a, b)
+	fmt.Println("After SWAP value of A:", a)
+	fmt.Println("After SWAP value of B:", b)
+}
+func swap(x, y int) int {
+	var temp int
+	temp = x
+	x = y
+	y = temp
+	return temp
+}
